73d1263da4f72c511439ae83fe6593012e111f02
Manuel Mancipe <manuel-mancipe@hotmail.com>
2018-11-05 20:24:53 -0500

    primer avance proyecto objetos

---
+++ ProyectoObjetos/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoObjetos
+{
+    class Persona
+    {
+        private String nombre;
+        private String profesion;
+        private int edad;
+    }
+}
